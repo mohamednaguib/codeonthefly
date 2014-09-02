53228c2d0336eea2aab200e8b2df54b98d81c137
mohamednaguib <mohamed.naguib@socialfruits.com>
2014-09-02 18:58:24 +0300

    Update DocumentDBSample.cs
    delete id

--- samples/DocumentDBSample.cs
+++ samples/DocumentDBSample.cs
@@ -56,7 +56,6 @@ namespace DocumentDbSample
     }
     public class Employee
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
